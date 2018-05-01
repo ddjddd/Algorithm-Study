7eef74b967f626fde9489d6e36609a1cc091b372
jinho <ddjddd@naver.com>
2018-05-01 09:50:33 +0900

    18.4.30

---
+++ Backjoon_Onlie_Judge/1000~/1850.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+typedef unsigned long long ll;
+
+ll gcd(ll a, ll b) {
+	if(b == 0) return a;
+	
+	return gcd(b, a%b);
+}
+
+int main() {
+	ll a, b, g;
+	cin >> a >> b;
+	g = gcd(a, b);
+	for(ll i = 0; i < g; i++) {
+		cout << '1';
+	}
+}
