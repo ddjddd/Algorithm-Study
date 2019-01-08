ea6c27de2a2b68b8a31d92bebd662aa5c30011a3
ddjddd <ddjddd@naver.com>
2019-01-08 15:48:25 +0900

    `19.1.8

---
+++ Backjoon_Onlie_Judge/1000~/1032.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	int tc; cin >> tc;
+	string arr[50];
+	for(int i = 0; i < tc; i++) {
+		cin >> arr[i];
+	}
+	int len = arr[0].size();
+	for(int i = 0; i < len; i++) {
+		bool flag = true;
+		char tmp = arr[0][i];
+		for(int j = 1; j < tc; j++) {
+			if(tmp != arr[j][i]) {
+				flag = false;
+				break;
+			}
+		}
+		if(!flag) cout << '?';
+		else cout << arr[0][i];
+	}
+	cout << '\n';
+	
+	return 0;
+}
