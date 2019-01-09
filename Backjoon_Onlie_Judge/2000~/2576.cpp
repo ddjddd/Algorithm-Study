b4a99c9aee27e699298c37cc6e5640d2747ce4d1
ddjddd <ddjddd@naver.com>
2019-01-09 23:59:09 +0900

    `19.01.09

---
+++ Backjoon_Onlie_Judge/2000~/2576.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int tmp = 0, ret = 0, m = 1000;
+	for(int i = 0; i < 7; i++)  {
+		cin >> tmp;
+		if(tmp&1) {
+			ret += tmp;
+			m = min(m, tmp);
+		}
+	}
+	if(ret == 0) cout << "-1\n";
+	else cout << ret << '\n' << m << '\n';
+}
