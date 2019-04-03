92c45118c004d255e4a627962a1e148eecdcd09b
ddjddd <ddjddd@naver.com>
2019-04-03 18:54:09 +0900

    `19.04.03

---
+++ Backjoon_Onlie_Judge/2000~/2312.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+
+using namespace std;
+
+vector <int> prime;
+vector <int> :: iterator it;
+
+void prime_num() {
+	bool check[1001] = {false, };
+	for(int i = 2; i <= 1000; i++) {
+		if(check[i]) continue;
+		prime.push_back(i);
+		for(int j = i*i; j <= 1000; j += i)
+			check[j] = true;
+	}
+}
+
+void solve(int input) {
+	
+	for(it = prime.begin(); it != prime.end(); it++) {
+		int cnt = 0, tmp = *it;
+		while(!(input % tmp)) {
+			cnt++;
+			input /= tmp;
+		}
+		if(cnt) printf("%d %d\n", tmp, cnt);
+	}
+	if(input != 1) printf("%d 1\n", input);
+}
+
+int main () {
+	prime_num();
+	int tC; cin >> tC;
+	for(int tc = 1; tc <= tC; tc++) {
+		int t;
+		cin >> t;
+		solve(t);
+	}
+	
+	return 0;
+}
---
+++ Backjoon_Onlie_Judge/2000~/2447.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main () {
+	int n;
+	cin >> n;
+	char** arr = new char*[n];
+	for(int i = 0; i < n; i++) {
+		arr[i] = new char[n];
+		for(int j = 0; j < n; j++) {
+			arr[i][j] = '*';
+		}
+	}
+	
+	int m = n/3, tmp = 1;
+	
+	while(m != 0) {
+		for(int i = 0; i < tmp; i++) {
+		for(int j = 0; j < tmp; j++) {
+			int x = 3 * m * i + m, y = 3 * m * j + m;
+			int dx = x + m, dy = y + m;
+			for(int ii = x; ii < dx; ii++) {
+			for(int jj = y; jj < dy; jj++){
+				arr[ii][jj] = ' ';
+			}
+			}
+		}
+		}
+		tmp *= 3;
+		m /= 3;
+	}
+	
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			printf("%c", arr[i][j]);
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
