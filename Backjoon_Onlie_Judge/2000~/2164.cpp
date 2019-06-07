6882fb9486f83bbbb9d6b00c7df497ceb581f8c0
ddjddd <ddjddd@naver.com>
2019-06-07 17:21:56 +0900

    `19.06.07

---
+++ Backjoon_Onlie_Judge/2000~/2164.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+using namespace std;
+
+int main () {
+	int n, ret = 0;
+	scanf("%d", &n);
+	
+	int i = 1;
+	
+	while((i << 1) <= n) {
+		i = i << 1;
+	}
+	
+	if(i == n) ret = i;
+	else ret = (n-i)<<1;
+	printf("%d\n", ret);
+		
+	return 0;
+}
