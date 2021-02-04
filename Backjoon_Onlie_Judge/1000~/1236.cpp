061f2508192119c7ba4d3b6d78e6ceef48e1259f
Jinho.Choi <ddjddd@naver.com>
2021-02-04 21:26:41 +0900

    2020.02.04

---
+++ Backjoon_Onlie_Judge/1000~/1236.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+char map[51][51];
+int n,m;
+bool x[51] = {0, }, y[51] = {0, };
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> map[i];
+    }
+
+    int a = 0, b = 0;
+    for (int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(map[i][j] == 'X') x[i] = true, y[j] = true;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if(!x[i]) a++;
+    }
+    for (int i = 0; i < m; i++) {
+        if(!y[i]) b++;
+    }
+
+    cout << (a>b?a:b) << endl;
+
+    return 0;
+}
