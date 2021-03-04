b8f64cb5d836a3eea42b16a56c580463259f8789
Jinho.Choi <ddjddd@naver.com>
2021-03-04 19:47:12 +0900

    2020.03.04

---
+++ Backjoon_Onlie_Judge/10000~/10707.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int a, b, c, d, p, ret, ta, tb; cin >> a >> b >> c >> d >> p;
+    ta = a*p;
+    tb = b + (c<p ? (p-c)*d : 0);
+    cout << (ta<tb ? ta : tb) << '\n';
+    return 0;
+}
---
+++ Backjoon_Onlie_Judge/19000~/19698.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int a, b, c, d, ret; cin >> a >> b >> c >> d;
+    ret = (b/d)*(c/d);
+    cout << (ret > a ? a : ret) << '\n';
+    return 0;
+}
