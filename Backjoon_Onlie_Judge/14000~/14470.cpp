a60f9c647bea7d09a9c4a366e77dd2979e0746b4
Jinho.Choi <ddjddd@naver.com>
2021-03-08 22:02:34 +0900

    2021.03.08

---
+++ Backjoon_Onlie_Judge/14000~/14470.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int a,b,c,d,e, ret;
+    cin >> a >> b >> c >> d >> e;
+    ret = (a<0 ? -a*c + d : 0) + (a>0 ? (b-a)*e : b*e);
+    cout << ret;
+    return 0;
+}
