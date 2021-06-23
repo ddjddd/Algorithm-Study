27597ce6b1d68f52f33be24379bd168b19281e59
Jinho.Choi <ddjddd@naver.com>
2021-06-23 21:59:18 +0900

    2021.06.22

---
+++ Backjoon_Onlie_Judge/1000~/1072.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long x, y; cin >> x >> y;
+    long long z = y*100/x;
+
+    int ans = -1;
+    if (z < 99) {
+        int left = 0, right = 1000000000;
+
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            int tmp = (y+mid)*100 / (x+mid);
+
+            if(tmp <= z) {
+                ans = mid + 1;
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+    }
+    cout << ans << '\n';
+
+    return 0;
+}
---
+++ Backjoon_Onlie_Judge/2000~/2034.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+typedef pair<int, int> pii;
+
+int main() {
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+
+    int n; cin >> n;
+    vector <int> v(n);
+    for(int i = 0; i < n; i++) cin >> v[i];
+
+    for(char c = 'A'; c != 'H'; c++) {
+        char cur = c;
+
+        bool flag = true;
+        for(auto& it: v) {
+            int tmp = it;
+
+            while(tmp && flag) {
+                if(tmp < 0) {
+                    tmp += (cur == 'C' || cur == 'F') ? 1 : 2;
+                    cur--;
+                    if (cur < 'A') cur += 7;
+                    if(tmp > 0) flag = false;
+                }
+                else if (tmp > 0) {
+                    tmp -= (cur == 'B' || cur == 'E') ? 1 : 2;
+                    cur++;
+                    if (cur > 'G') cur -= 7;
+                    if(tmp < 0) flag = false;
+                }
+            }
+            if(!flag) break;
+        }
+        if(flag) {
+            cout << c << ' ' << cur << '\n';
+        }
+    }
+
+    return 0;
+}
